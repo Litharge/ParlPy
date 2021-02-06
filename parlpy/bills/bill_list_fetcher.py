b2641365438677324af137e4884f0c9f74b32358
Robert <r@u1.home>
2021-02-06 03:00:28 +0000

    feat: add support for different sessions

--- parlpy/bills/bill_list_fetcher.py
+++ parlpy/bills/bill_list_fetcher.py
@@ -16,36 +16,56 @@ class BillFetcher():
         # todo:
         #   member var ?:bills_df_last_updated
         #   member var dataframe:bill_data
-        self.bills_overview_scheme = "https"
-        self.bills_overview_netloc = "bills.parliament.uk"
+        #   one for each session and one containing intersection?
+
+        self.__bills_overview_scheme = "https"
+        self.__bills_overview_netloc = "bills.parliament.uk"
+
+        self.__bills_overview_session = {"2019 - 21": "35", "2019 - 19": "34", "2017 - 19": "33"}
+        #self.__bills_overview_sort_by = {"Title (A-Z)": "0"}
+
         pass
 
     # get the total number of pages of bills for the selected session
     # there are many other possible methods of doing this - uncertain which is most robust
     def determine_number_pages_for_session(self, session):
+        page_query_string = urllib.parse.urlencode(
+            OrderedDict(
+                Session=session
+            )
+        )
+
+
         url = urllib.parse.urlunparse((
-            self.bills_overview_scheme, self.bills_overview_netloc, "", "", "", ""
+            self.__bills_overview_scheme, self.__bills_overview_netloc, "", "", page_query_string, ""
         ))
+        print("url: {}".format(url))
 
         html_data = urlopen(url)
         data_bs = BeautifulSoup(html_data.read(), 'html.parser')
-        page_selection_elements = data_bs.find_all("a", href=re.compile("/\?page=*"))
+        page_selection_elements = data_bs.find_all("a", href=re.compile("page=*"))
 
         max_page = 1 # there is always at least one page for a valid session
         for pt in page_selection_elements:
-            page = int(pt["href"].split('=')[1])
+            page = int(pt["href"].split('page=')[1])
 
             if page > max_page:
                 max_page = page
 
         return max_page
 
-    def fetch_all_titles_on_page(self, page):
+    def fetch_all_titles_on_page(self, session, page):
         # todo: put scheme, netloc, query mappings in member vars (or utils? - bc these macros will likely need to be
         #   used externally by api users...)
-        page_query_string = urllib.parse.urlencode(OrderedDict(page=str(page)))
+        page_query_string = urllib.parse.urlencode(
+            OrderedDict(
+                Session=session,
+                page=str(page),
+            )
+        )
+
         url = urllib.parse.urlunparse((
-            self.bills_overview_scheme, self.bills_overview_netloc, "", "", page_query_string, ""
+            self.__bills_overview_scheme, self.__bills_overview_netloc, "", "", page_query_string, ""
         ))
 
         html_data = urlopen(url)
@@ -62,8 +82,9 @@ class BillFetcher():
     # method to fetch overview information about all bills
     # currently fetches all bill titles in current session
     def fetch_all_bills(self):
-        max_page = self.determine_number_pages_for_session(1)
+        session = self.__bills_overview_session["2019 - 19"]
+        max_page = self.determine_number_pages_for_session(session)
 
         for i in range(1, max_page+1):
             time.sleep(1)
-            self.fetch_all_titles_on_page(i)
+            self.fetch_all_titles_on_page(session, i)
