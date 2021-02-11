05e64d3899d48fd191c4107e45d4991fa89b1629
Robert <r@u1.home>
2021-02-11 14:40:57 +0000

    fix: display all cols

--- parlpy/test/test_bills_bill_list_fetcher.py
+++ parlpy/test/test_bills_bill_list_fetcher.py
@@ -1,8 +1,13 @@
+import pandas as pd
+
 import parlpy.bills.bill_list_fetcher
 
 test_fetcher = parlpy.bills.bill_list_fetcher.BillsOverview()
 
 test_fetcher.update_all_bills_in_session()
+
+# display all columns of dataframe
+pd.set_option("display.max_columns", len(test_fetcher.bills_overview_data.columns))
 
 print(test_fetcher.bills_overview_data)
 
