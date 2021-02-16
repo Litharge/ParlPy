33d8f7bfa6431e086b62582913601be5cc1ed978
Robert <r@u1.home>
2021-02-16 15:53:24 +0000

    test: convert to unit test, add type tests

--- parlpy/test/test_bills_bill_list_fetcher.py
+++ parlpy/test/test_bills_bill_list_fetcher.py
@@ -1,14 +1,42 @@
 import pandas as pd
+import datetime
+import numpy as np
 
 import parlpy.bills.bill_list_fetcher
 
-test_fetcher = parlpy.bills.bill_list_fetcher.BillsOverview()
+import unittest
 
-test_fetcher.update_all_bills_in_session()
 
-# display all columns of dataframe
-pd.set_option("display.max_columns", len(test_fetcher.bills_overview_data.columns))
 
-print(test_fetcher.bills_overview_data)
+class TestOverview(unittest.TestCase):
+    # create BillsOverview object ready for tests
+    # also print result
+    def setUp(self):
+        test_fetcher = parlpy.bills.bill_list_fetcher.BillsOverview()
+        test_fetcher.update_all_bills_in_session()
 
-print(test_fetcher.bills_overview_data[test_fetcher.bills_overview_data.bill_title == "Fire Safety Bill"])
+        self.test_fetcher = test_fetcher
+
+        pd.set_option("display.max_columns", len(self.test_fetcher.bills_overview_data.columns))
+
+        print(self.test_fetcher.bills_overview_data)
+
+        print(self.test_fetcher.bills_overview_data[
+                  self.test_fetcher.bills_overview_data.bill_title == "Fire Safety Bill"
+                  ])
+
+    # test types of dataframe
+    def test_dataframe_types(self):
+        #self.assertIsInstance(self.test_fetcher.bills_overview_data, pd.DataFrame)
+        print(self.test_fetcher.bills_overview_data.dtypes)
+        # strings are stored as objects in dataframes
+        self.assertTrue(self.test_fetcher.bills_overview_data.bill_title.dtype == object)
+
+        print("dtype value")
+        print(self.test_fetcher.bills_overview_data.last_updated.dtype)
+
+        self.assertTrue(self.test_fetcher.bills_overview_data.last_updated.dtype == np.dtype('datetime64[ns]'))
+        self.assertTrue(self.test_fetcher.bills_overview_data.bill_detail_path.dtype == object)
+
+if __name__ == '__main__':
+    unittest.main()
