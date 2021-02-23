a59e0be7c367cc6432bd2bdbfbaa9591ded12729
Robert <r@u1.home>
2021-02-23 15:55:24 +0000

    test: reorganise tests so that both major methods are tested

--- parlpy/test/test_bills_bill_list_fetcher.py
+++ parlpy/test/test_bills_bill_list_fetcher.py
@@ -14,28 +14,34 @@ class TestOverview(unittest.TestCase):
     # also print result
     def setUp(self):
         self.test_fetcher = BillsOverview(debug=False)
-        #test_fetcher.update_all_bills_in_session()
 
         pd.set_option("display.max_columns", len(self.test_fetcher.bills_overview_data.columns))
 
         #print(self.test_fetcher.bills_overview_data)
 
+    # check that the second call to get_changed_bills_in_session puts more or equal items into bill_overview_data
+    # than the first
     def test_update_only_needed(self):
         self.test_fetcher.get_changed_bills_in_session()
         print("dataframe:")
         print(self.test_fetcher.bills_overview_data)
+        print("---------------------------------------------------")
         df_row_count_1 = len(self.test_fetcher.bills_overview_data.index)
 
         self.test_fetcher.get_changed_bills_in_session()
         print("dataframe:")
         print(self.test_fetcher.bills_overview_data)
+        print("---------------------------------------------------")
         df_row_count_2 = len(self.test_fetcher.bills_overview_data.index)
 
-        self.assertTrue(df_row_count_1 > df_row_count_2)
+        self.assertTrue(df_row_count_1 >= df_row_count_2)
 
-    """
     # test types of dataframe
     def test_dataframe_types(self):
+        self.test_fetcher.update_all_bills_in_session()
+        print(self.test_fetcher.bills_overview_data)
+        print("---------------------------------------------------")
+
         self.assertIsInstance(self.test_fetcher.bills_overview_data, pd.DataFrame)
 
         print(self.test_fetcher.bills_overview_data.dtypes)
@@ -46,7 +52,6 @@ class TestOverview(unittest.TestCase):
 
         # check that last_updated is stored as datetime64[ns]
         self.assertTrue(self.test_fetcher.bills_overview_data.last_updated.dtype == np.dtype('datetime64[ns]'))
-    """
 
 
     """
@@ -65,8 +70,6 @@ class TestOverview(unittest.TestCase):
 
         print(self.test_fetcher.bills_overview_data)
     """
-
-    # todo: create hand made bills html data to check that update is working
 
 
 
