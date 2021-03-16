d38da96b3742744905558412f00a9d181c6b0b61
Robert <1robertchambers@gmail.com>
2021-03-16 21:52:23 +0000

    test: print all columns of dataframe

--- parlpy/test/test_mp_fetcher.py
+++ parlpy/test/test_mp_fetcher.py
@@ -1,5 +1,8 @@
-from parlpy.mps.mp_fetcher import MPOverview
 import json
+import pandas as pd
+
+from parlpy.mps.mp_fetcher import MPOverview
+
 
 
 def jprint(obj):
@@ -12,4 +15,5 @@ mp = MPOverview()
 # mp.get_active_MPs(verbose=True)
 mp.get_all_members(params={"IsCurrentMember": True, "House": "Commons", "skip": 620}, verbose=True)
 
+pd.set_option("display.max_columns", len(mp.mp_overview_data.columns))
 print(mp.mp_overview_data)
