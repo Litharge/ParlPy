d3261fc785c4b802606df0d4565acb5be691efb9
Robert <1robertchambers@gmail.com>
2021-03-16 17:28:38 +0000

    feat: add code to iterator to yield summary

--- parlpy/test/test_bills_bill_details_iterator.py
+++ parlpy/test/test_bills_bill_details_iterator.py
@@ -1,5 +1,12 @@
 import unittest
 
+import parlpy.bills.bill_list_fetcher as blf
+import parlpy.bills.bill_details_iterator as bdi
+
 class TestDetails(unittest.TestCase):
     def test_iterator_on_2004_05_session(self):
-        pass
+        test_fetcher = blf.BillsOverview()
+        test_fetcher.update_all_bills_in_session(session_name="2004-05")
+
+        for s in bdi.get_bill_details(test_fetcher):
+            print(s)
