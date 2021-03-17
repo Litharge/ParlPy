b30e3d76e423f06e733874370b864e88b8a59e7f
Robert <1robertchambers@gmail.com>
2021-03-17 18:49:08 +0000

    feat: narrow division search by sessions

--- parlpy/bills/bill_votes_fetcher.py
+++ parlpy/bills/bill_votes_fetcher.py
@@ -5,10 +5,16 @@ Robert Chambers 2021
 import requests
 import datetime
 
-def fetch_votes(bill_title: str, start_datetime: datetime.date, end_datetime: datetime.date) -> requests.Request:
+def fetch_votes(bill_title: str, start_datetime: datetime.date, end_datetime: datetime.date = None) -> requests.Request:
     votes_endpoint = 'https://commonsvotes-api.parliament.uk/data/divisions.json/search'
 
-    votes_parameters = {'queryParameters.searchTerm': bill_title}
+    votes_parameters = {'queryParameters.searchTerm': bill_title,
+                        'queryParameters.startDate': start_datetime.isoformat(),
+                        }
+
+    # add end time to narrow results, unless end_datetime None, which is the case for the current session only
+    if end_datetime is not None:
+        votes_parameters['queryParameters.endDate': end_datetime.isoformat()]
 
     r = requests.get(votes_endpoint, params=votes_parameters)
 
