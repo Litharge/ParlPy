4876f8c08fdd1e60c0ca58b3df736c75ad39cb98
Robert <1robertchambers@gmail.com>
2021-03-17 18:59:09 +0000

    test: add time delimiters for test item

--- parlpy/test/test_bill_votes_fetcher.py
+++ parlpy/test/test_bill_votes_fetcher.py
@@ -1,9 +1,15 @@
 import unittest
+import datetime
 
 from parlpy.bills.bill_votes_fetcher import fetch_votes
 
+
+
 class TestVotes(unittest.TestCase):
     def test_votes_for_single_bill(self):
-        r = fetch_votes('financial services')
+        mock_start_time = datetime.date(2019,12,9)
+        mock_end_time = None
+
+        r = fetch_votes('financial services', mock_start_time, mock_end_time)
 
         print(r.json())
