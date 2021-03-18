f3e4f82831d3737cf6deba9517734255fb97791b
Robert <1robertchambers@gmail.com>
2021-03-18 17:17:20 +0000

    feat: define class structure for holding party info

---
+++ parlpy/mps/parties_fetcher.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+class PartiesInformation():
+    def __init__(self, party_name, party_id):
+        self.party_name = party_name
+        self.party_id = party_id
+
+
