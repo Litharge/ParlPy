3c296bf6dc47eb6a2fb0a6a6b37bafa416bc3c08
Robert <1robertchambers@gmail.com>
2021-03-18 17:39:17 +0000

    feat: add function to get list of PartyInformation objects containing id and name of party

--- parlpy/mps/parties_fetcher.py
+++ parlpy/mps/parties_fetcher.py
@@ -1,9 +1,26 @@
 import requests
 import json
 
-class PartiesInformation():
+class PartyInformation():
     def __init__(self, party_name, party_id):
         self.party_name = party_name
         self.party_id = party_id
 
+def get_all_parties():
+    commons_parties_endpoint = "https://members-api.parliament.uk/api/Parties/GetActive/Commons"
 
+    r = requests.get(commons_parties_endpoint)
+    parties_object = json.loads(r.text)
+
+    parties_items_object = parties_object["items"]
+
+    # list holding PartiesInformation objects
+    parties_list = []
+    for p in parties_items_object:
+        party_id = p["value"]["id"]
+        party_name = p["value"]["name"]
+
+        party_obj = PartyInformation(party_name, party_id)
+        parties_list.append(party_obj)
+
+    return parties_list
