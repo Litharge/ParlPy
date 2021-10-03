9943d89fc789c619cb9e048bc2f2147db99ffa98
Robert Chambers <1robertchambers@gmail.com>
2021-10-03 08:45:44 +0100

    test: that details iterator gets the correct stripped title

--- parlpy/test/test_bills_bill_details_iterator.py
+++ parlpy/test/test_bills_bill_details_iterator.py
@@ -14,6 +14,14 @@ def print_all_info_using_iterator(fetcher):
             print(f"noes: {d.noes}")
 
 
+def get_last_bill_details(fetcher) -> BillDetails:
+    last_item = None
+    for s in bdi.get_bill_details(fetcher, verbose=False):
+        last_item = s
+
+    return last_item
+
+
 class TestDetails(unittest.TestCase):
     # a much shorter test, but does not test divisions capabilities
     def test_print_iterator_on_2004_05_session(self):
@@ -33,4 +41,14 @@ class TestDetails(unittest.TestCase):
 
         print_all_info_using_iterator(fetcher_2019_21)
 
+    def test_correct_bill_stripped_title_obtained(self):
+        fetcher_2019_21 = blf.BillsOverview()
+
+        mock_datetime = datetime.datetime(2021, 9, 16, 18)
+        fetcher_2019_21.mock_datetime_last_scraped(mock_datetime)
+        fetcher_2019_21.get_changed_bills_in_session(session_name="2019-21")
+
+        last_bill = get_last_bill_details(fetcher_2019_21)
+
+        self.assertTrue(last_bill.title_stripped == "Telecommunications (Security)")
 
